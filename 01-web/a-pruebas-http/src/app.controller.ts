389710316184a21daaf3148c00466de6316c95c6
nada <nada>
2019-11-28 16:09:54 -0500

    commit clase 9

--- 01-web/a-pruebas-http/src/app.controller.ts
+++ 01-web/a-pruebas-http/src/app.controller.ts
@@ -1,16 +1,117 @@
+<<<<<<< Updated upstream
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+=======
+import {
+    Body,
+    Controller,
+    Get, Headers,
+    HttpCode,
+    InternalServerErrorException,
+    Param,
+    Post,
+    Query,
+} from '@nestjs/common';
+import {AppService} from './app.service';
+import {get} from 'http';
+>>>>>>> Stashed changes
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+<<<<<<< Updated upstream
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+=======
+    // http://localhost:3000/pepito/hola-mundo
+    @Get('hola-mundo')// -> url "/"
+    getHello(): string {
+        return 'Hola mundo';
+    }
+
+    @HttpCode(200)
+    @Post('EsPar')
+    adiosMundo(): string {
+        const segundos = this.obtenerSegundos();
+        if (segundos % 2 === 0) {
+            return 'Adios Mundo!';
+        } else {
+            throw new InternalServerErrorException('Es impar');
+        }
+
+    }
+
+    private obtenerSegundos(): number {
+        return new Date().getSeconds();
+    }
+    @Get('bienvenida')
+    public bienvenida(
+        @Query() parametrosDeConsulta: ObjetoBienvenida,
+        @Query('nombre') nombreUsuario: string,
+        @Query('numero') numeroUsuario: number,
+        @Query('casado') casadoUsuario: boolean,
+    ): string {
+        // tslint:disable-next-line:no-console
+        console.log(parametrosDeConsulta);
+        // tslint:disable-next-line:no-console
+        console.log(typeof nombreUsuario);
+        // tslint:disable-next-line:no-console
+        console.log(typeof numeroUsuario);
+        // tslint:disable-next-line:no-console
+        console.log(typeof casadoUsuario);
+        // template strings \\ 'Mensaje ${variable}'
+        return 'Mensaje ${parametrosDeConsulta.nombre} Numero: ${parametrosDeConsulta.numero}';
+    }
+
+    @Get('inscripcion-curso/:idCurso/:cedula') // /:nombreParametro
+    public inscripcionCurso(
+        @Param() parametrosDeRuta: ObjetoInscripcion,
+        @Param('idCurso') idCurso: string,
+        // tslint:disable-next-line:no-shadowed-variable
+        @Param('cedula') cedula: string,
+    ): string {
+        // tslint:disable-next-line:no-console
+        console.log(parametrosDeRuta);
+        // template strings \\ 'Mensaje ${variable}'
+        return 'Te inscribiste al curso: ${parametrosDeRuta.idCurso}' +
+            '${parametrosDeRuta.cedula}';
+    }
+    @Post('almorzar')
+    @HttpCode(200)
+    public almorzar(
+        @Body() parametrosDeCuerpo,
+        @Body('id') id: number,
+    ): string {
+        // tslint:disable-next-line:no-console
+        console.log(parametrosDeCuerpo);
+        // template strings \\ 'Mensaje ${variable}'
+        return 'El id es: ${id}';
+    }
+    @Get('obtener-cabeceras')
+    obtenerCabeceras(
+        @Headers() cabeceras,
+        @Headers('numerouno') numeroUno: string,
+    ) {
+        // tslint:disable-next-line:no-console
+        console.log(cabeceras);
+        return 'Las cabeceras son ${numeroUno}';
+    }
+
+>>>>>>> Stashed changes
 }
 
+interface ObjetoBienvenida {
+    idCurso?: string;
+    cedula: string;
+}
+interface ObjetoInscripcion {
+    nombre?: string;
+    numero?: string;
+    casado?: string;
+}
 // Typescript
 // Declaracion de variables
 // No utilizar
