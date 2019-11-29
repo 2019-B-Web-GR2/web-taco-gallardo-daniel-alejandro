5b6cfc8f0d8cc3708843e65f69e762d0975d948a
Daniel Taco <daniel.taco@epn.edu.ec>
2019-11-29 16:03:56 -0500

    clase 10

--- 01-web/a-pruebas-http/src/app.module.ts
+++ 01-web/a-pruebas-http/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmCoreModule} from '@nestjs/typeorm/dist/typeorm-core.module';
+import {UsuarioEntity} from './usuario/usuario.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmCoreModule.forRoot(
+      {type: 'mysql',
+        host: 'localhost',
+        port: 32783,
+        username: 'daniel',
+        password: '1234',
+        database: 'web',
+        entities: [
+            UsuarioEntity,
+        ],
+        synchronize: true},
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
